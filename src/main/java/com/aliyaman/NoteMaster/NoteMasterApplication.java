6cb4376d067cbd85658eb55d13eee2e2bca87a63
alimrtyaman <aliyaman0110@gmail.com>
2025-07-29 20:57:04 +0200

    project set up

---
+++ src/main/java/com/aliyaman/NoteMaster/NoteMasterApplication.java
@@ -0,0 +1,13 @@
+package com.aliyaman.NoteMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliyaman/NoteMaster/NoteMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliyaman.NoteMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
